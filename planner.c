030c82992516b976f0c3cb606a61a5b4580624c7
hangsman <hi@hangsman.cn>
2025-08-26 11:16:35 +0800

    Fix feed override bypass when programmed rate exceeds rapid rate

--- planner.c
+++ planner.c
@@ -313,6 +313,8 @@ float plan_compute_profile_nominal_speed (plan_block_t *block)
     if(block->condition.rapid_motion)
         nominal_speed *= (0.01f * (float)sys.override.rapid_rate);
     else {
+		if(nominal_speed > block->rapid_rate)
+            nominal_speed = block->rapid_rate;
         if(!block->condition.no_feed_override)
             nominal_speed *= (0.01f * (float)sys.override.feed_rate);
         if(nominal_speed > block->rapid_rate)
